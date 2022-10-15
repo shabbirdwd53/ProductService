9142b682797ff89d4c5cb4db846c4195a3ada303
Shabbir Dawoodi <shabbirdwd53@gmail.com>
2022-10-15 12:00:44 +0530

    Initial Setup

---
+++ src/test/java/com/dailycodebuffer/ProductService/ProductServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.dailycodebuffer.ProductService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
